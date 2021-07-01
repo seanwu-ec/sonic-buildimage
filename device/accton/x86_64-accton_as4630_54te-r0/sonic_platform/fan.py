9bef63a13f840ddc5628209af2741a7a0b4dfd56
Sean Wu <sean_wu@edge-core.com>
2021-07-01 06:43:06 +0000

    as4630-54te support system health
    1. Implement SysLed on chassis.
    2. Correct fan number, fan.get_direction() and get_presence()
    3. Implement required fan APIs for SystemHealth to check fan.speed
    4. Correct LED registers.
    
    Signed-off-by: Sean Wu <sean_wu@edge-core.com>

--- device/accton/x86_64-accton_as4630_54te-r0/sonic_platform/fan.py
+++ device/accton/x86_64-accton_as4630_54te-r0/sonic_platform/fan.py
@@ -8,11 +8,13 @@
 
 try:
     from sonic_platform_base.fan_base import FanBase
+    from sonic_platform_base.fan_drawer_base import FanDrawerBase
     from .helper import APIHelper
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
 PSU_FAN_MAX_RPM = 26688
+FAN_MAX_RPM = 13000
 
 CPLD_I2C_PATH = "/sys/bus/i2c/devices/3-0060/fan_"
 PSU_HWMON_I2C_PATH ="/sys/bus/i2c/devices/{}-00{}/"
@@ -25,6 +27,12 @@ PSU_I2C_MAPPING = {
         "num": 11,
         "addr": "59"
     },
+}
+FANLED_FNODE = "/sys/class/leds/fan/brightness"
+FANLED_MODES = {
+    "0": FanBase.STATUS_LED_COLOR_OFF,
+    "1": FanBase.STATUS_LED_COLOR_GREEN,
+    "2": FanBase.STATUS_LED_COLOR_AMBER
 }
 
 
@@ -57,10 +65,10 @@ class Fan(FanBase):
 
 
         if not self.is_psu_fan:
-            dir_str = "{}{}{}".format(CPLD_I2C_PATH, 'direction_', self.fan_tray_index)
+            dir_str = "{}{}{}".format(CPLD_I2C_PATH, 'direction_', self.fan_tray_index+1)
             val=self._api_helper.read_txt_file(dir_str)
             if val is not None:
-                if val==0:#F2B
+                if val=='0':#F2B
                     direction=self.FAN_DIRECTION_EXHAUST
                 else:
                     direction=self.FAN_DIRECTION_INTAKE
@@ -93,16 +101,19 @@ class Fan(FanBase):
             psu_fan_path= "{}{}".format(self.psu_hwmon_path, 'psu_fan1_speed_rpm')
             fan_speed_rpm = self._api_helper.read_txt_file(psu_fan_path)
             if fan_speed_rpm is not None:
-                speed = (int(fan_speed_rpm,10))*100/26688
+                speed = (int(fan_speed_rpm,10))*100/PSU_FAN_MAX_RPM
                 if speed > 100:
                     speed=100
             else:
                 return 0
         elif self.get_presence():            
-            speed_path = "{}{}".format(CPLD_I2C_PATH, 'duty_cycle_percentage')
-            speed=self._api_helper.read_txt_file(speed_path)
-            if speed is None:
+            speed_path = "{}{}{}".format(CPLD_I2C_PATH, 'speed_rpm_', self.fan_tray_index+1)
+            speed_rpm=self._api_helper.read_txt_file(speed_path)
+            if speed_rpm is None:
                 return 0
+            else:
+                speed_rpm = int(speed_rpm)
+                speed = speed_rpm*100/FAN_MAX_RPM if speed_rpm < FAN_MAX_RPM else 100
         return int(speed)
 
     def get_target_speed(self):
@@ -116,7 +127,14 @@ class Fan(FanBase):
             0   : when PWM mode is use
             pwm : when pwm mode is not use
         """
-        return False #Not supported
+        if self.is_psu_fan:
+            raise NotImplementedError
+        if not self.get_presence():
+            return 0
+
+        speed_path = "{}{}".format(CPLD_I2C_PATH, 'duty_cycle_percentage')
+        speed = self._api_helper.read_txt_file(speed_path)
+        return int(speed) if speed is not None else 0
 
     def get_speed_tolerance(self):
         """
@@ -125,7 +143,7 @@ class Fan(FanBase):
             An integer, the percentage of variance from target speed which is
                  considered tolerable
         """
-        return False #Not supported
+        return 80
 
     def set_speed(self, speed):
         """
@@ -154,18 +172,66 @@ class Fan(FanBase):
         """
         return False #Not supported
 
+    def get_status_led(self):
+        """
+        Gets the state of the fan status LED
+
+        Returns:
+            A string, one of the predefined STATUS_LED_COLOR_* strings above
+        """
+        if self.is_psu_fan:
+            raise NotImplementedError
+        read_val=self._api_helper.read_txt_file(FANLED_FNODE)
+        return FANLED_MODES[read_val] if read_val in FANLED_MODES else "unknown"
+
     def get_presence(self):
         """
         Retrieves the presence of the FAN
         Returns:
             bool: True if FAN is present, False if not
         """
-        present_path = "{}{}{}".format(CPLD_I2C_PATH, 'present_', self.fan_index+1)
-        val=self._api_helper.read_txt_file(present_path)
         if not self.is_psu_fan:
+            present_path = "{}{}{}".format(CPLD_I2C_PATH, 'present_', self.fan_tray_index+1)
+            val=self._api_helper.read_txt_file(present_path)
             if val is not None:
                 return int(val, 10)==1
             else:
                 return False
         else:
             return True
+
+    def get_status(self):
+        """
+        Retrieves the operational status of the device
+        Returns:
+            A boolean value, True if device is operating properly, False if not
+        """
+        if self.is_psu_fan:
+            psu_fan_path= "{}{}".format(self.psu_hwmon_path, 'psu_fan1_fault')
+            val=self._api_helper.read_txt_file(psu_fan_path)
+            if val is not None:
+                return int(val, 10)==0
+            else:
+                return False
+        else:
+            path = "{}{}{}".format(CPLD_I2C_PATH, 'fault_', self.fan_tray_index+1)
+            val=self._api_helper.read_txt_file(path)
+            if val is not None:
+                return int(val, 10)==0
+            else:
+                return False
+
+    def get_name(self):
+        """
+        Retrieves the name of the device
+            Returns:
+            string: The name of the device
+        """
+        if not self.is_psu_fan:
+            fan_name = "FAN-{}".format(self.fan_tray_index+1)
+        else:
+            fan_name = "PSU-{} FAN-{}".format(self.psu_index+1, self.fan_index+1)
+        return fan_name
+
+class FanDrawer(FanDrawerBase):
+    pass
